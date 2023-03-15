a485bf9d28912de1024ab08d726258994737babc
Nikita Kisly <sour945@gmail.com>
2023-03-15 16:41:53 +0300

    refactor: rework router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 import { Layout } from '@/components/_index';
 import { NotFound } from '@/pages/_index';
@@ -6,16 +11,30 @@ import { NotFound } from '@/pages/_index';
 import { ROUTE } from '@/router/_constants';
 import routes from './routes';
 
-const AppRouter = () => {
-  return (
-    <Routes>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
       <Route path={ROUTE.home} element={<Layout />}>
         {routes.map(({ path, element }, key) => (
           <Route index={path === ROUTE.home} path={path} element={element} key={key} />
         ))}
       </Route>
       <Route path={ROUTE.notFound} element={<NotFound />} />
-    </Routes>
+    </>
+  )
+);
+
+const AppRouter = () => {
+  return (
+    <RouterProvider router={router} />
+    // <Routes>
+    //   <Route path={ROUTE.home} element={<Layout />}>
+    //     {routes.map(({ path, element }, key) => (
+    //       <Route index={path === ROUTE.home} path={path} element={element} key={key} />
+    //     ))}
+    //   </Route>
+    //   <Route path={ROUTE.notFound} element={<NotFound />} />
+    // </Routes>
   );
 };
 
