90d290979af98ebb5cb7d9083532a353015a4100
Nikita Kisly <sour945@gmail.com>
2023-04-07 12:30:11 +0300

    feat: update type guards

--- src/type-guards/photo-type-guard.ts
+++ src/type-guards/photo-type-guard.ts
@@ -1,11 +1,11 @@
 import { IPhoto } from '~services/unsplash/_types';
 
-const keys = ['id', 'likes', 'description', 'urls'] as (keyof IPhoto)[];
+const usedProps = ['id', 'likes', 'description', 'alt_description', 'urls'] as (keyof IPhoto)[];
 
 export function isPhoto(photo: unknown): photo is IPhoto {
   const predict = photo as IPhoto;
 
-  return keys.every((key) => {
+  return usedProps.every((key) => {
     if (Object.prototype.hasOwnProperty.call(predict, key)) {
       return true;
     } else {
