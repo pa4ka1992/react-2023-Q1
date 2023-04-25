59edf620ea8f3fa1629f74ba91adc1d78482882f
Nikita Kisly <sour945@gmail.com>
2023-04-25 11:40:36 +0300

    feat: update image preload

--- src/components/cards/Card/parts/Image/Image.tsx
+++ src/components/cards/Card/parts/Image/Image.tsx
@@ -1,4 +1,4 @@
-import { type Dispatch, type FC, type SetStateAction } from 'react';
+import { useEffect, useRef, type Dispatch, type FC, type SetStateAction } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from '../../Card.module.scss';
@@ -16,12 +16,20 @@ const { postDate, wrapImg, img } = styles;
 
 export const Image: FC<IImageProps> = ({ photo, setIsloaded }) => {
   const { id, urls, created_at } = photo;
+  const image = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (image.current?.complete) {
+      setIsloaded(true);
+    }
+  }, [image, setIsloaded]);
 
   return (
     <>
       <h4 className={postDate}>{formateDate(created_at)}</h4>
       <NavLink data-testid="card-link" to={`/photoID/${id}`} className={wrapImg}>
         <img
+          ref={image}
           data-testid="card-image"
           onLoad={() => setIsloaded(true)}
           className={img}
